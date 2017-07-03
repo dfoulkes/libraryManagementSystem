b425064a638b9826c5812b7be9a482584d0f43bb
musketeer91 <sec.tabrez03@gmail.com>
2017-07-04 00:18:39 +0600

    this is my attempt to create branch.[tabrez]

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,3 +1,7 @@
+package src;
+
+import java.util.ArrayList;
+
 /**
  * This will host as the runtime for the new app.
  * @author danfoulkes
@@ -5,10 +9,16 @@
  */
 public class LibraryApp {
 	
-	
+	private static ArrayList<String> booksName;
 	
 	public static void main(String[] args) {
 		
+            booksName = new ArrayList<>();
+            
 	}
+        
+        private void addBooks(String name){
+            booksName.add(name);
+        }
 
 }
